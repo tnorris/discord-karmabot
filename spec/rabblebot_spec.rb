7926bf6a5671f37c8f05d49c5d9d27ad8fe6c7ce
Tom Norris <tom.norris.iv@gmail.com>
2016-05-18 19:37:40 -0400

    appease travis

--- spec/rabblebot_spec.rb
+++ spec/rabblebot_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe RabbleBot do
   before(:each) do
-    @bot_invite_url = "https://lol.invalid/"
+    @bot_invite_url = 'https://lol.invalid/'
     bot_double = instance_double 'Discordrb::Bot'
     allow(bot_double).to receive(:info).and_return(nil)
     allow(bot_double).to receive(:debug).and_return(nil)
@@ -11,11 +11,11 @@ RSpec.describe RabbleBot do
 
   it 'can parse a configuration file' do
     rabble_bot = RabbleBot.new
-    expect{rabble_bot.load_config}.to_not raise_error
+    expect{ rabble_bot.load_config }.to_not raise_error
   end
 
   it 'can create a new object with the Discordrb::Bot mock' do
-    expect{RabbleBot.new}.to_not raise_error
+    expect{ RabbleBot.new }.to_not raise_error
   end
 
   it 'loads plugins' do
