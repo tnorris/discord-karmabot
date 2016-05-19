606f2f79a593bcf050322cf79b7671c45d4f44b7
Tom Norris <tom.norris.iv@gmail.com>
2016-05-18 21:44:23 -0400

    some refactoring for the weather plugin

--- lib/rabblebot/plugins/weather.rb
+++ lib/rabblebot/plugins/weather.rb
@@ -10,8 +10,8 @@ class RabbleBot
       def initialize(bot, config)
         super(bot, config)
         @bot.info 'Loading Weather Plugin'
-        @google_api = 'YOUR_GOOGLEAPI_KEY'
-        @forecastio_api = 'YOUR_FORECAST_IO_API_KEY'
+        @google_api = @config['google_api']
+        @forecastio_api = @config['forecastio_api']
         add_weather_handler
         @bot.info 'Weather Plugin loaded!'
       end
@@ -22,45 +22,24 @@ class RabbleBot
 
       def weather_query(e, message)
         gmaps_json_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{message}&key=#{@google_api}"
-        gmaps_json_response = open('#{gmaps_json_url}').read
-        gmaps_json = JSON.parse(gmaps_json_response)
+        gmaps_json_response = open(gmaps_json_url).read
+        begin
+          gmaps_json = JSON.parse(gmaps_json_response)
+        rescue StandardError => e
+          @bot.info "Something went wrong when parsing the Google Maps JSON. The error was #{e}"
+          return
+        end
         location = gmaps_json['results'][0]['formatted_address']
         lat = gmaps_json['results'][0]['geometry']['location']['lat']
         lng = gmaps_json['results'][0]['geometry']['location']['lng']
         forecast_io_url = "https://api.forecast.io/forecast/#{@forecastio_api}/#{lat},#{lng}"
-        forecast_io_response = open('#{forecast_io_url}').read
+        forecast_io_response = open(forecast_io_url).read
         forecast_io_json = JSON.parse(forecast_io_response)
-        icon = forecast_io_json['currently']['icon']
-        status = forecast_io_json['currently']['summary']
-        temp = forecast_io_json['currently']['temperature']
-        humidity = forecast_io_json['currently']['humidity'].to_f
-        humidity = (humidity * 100).to_i
-        windspeed = forecast_io_json['currently']['windSpeed']
-        cloudcover = forecast_io_json['currently']['cloudCover'].to_f
-        cloudcover = (cloudcover * 100).to_i
-        emoji = ''
-        case icon
-        when 'clear-night', 'clear-day'
-          emoji = ':waxing_gibbous_moon:'
-        when 'partly-cloudy-night', 'partly-cloudy-day'
-          emoji = ':partly_sunny:'
-        when 'rain'
-          emoji = ':cloud_rain:'
-        when 'snow'
-          emoji = ':cloud_snow:'
-        when 'sleet'
-          emoji = ':cloud_rain:'
-        when 'wind'
-          emoji = ':wind_blowing_face:'
-        when 'fog'
-          emoji = ':foggy:'
-        when 'cloudy'
-          emoji = ':cloud:'
-        when 'tornado'
-          emoji = ':cloud_tornado:'
-        when 'thunderstorm'
-          emoji = ':cloud_lightning:'
-        end
+
+        cloudcover, humidity, icon, status, temp, windspeed = parse_forecast_io_response(forecast_io_json)
+
+        emoji = lookup_emoji_for_weather_condition(icon)
+
         response = <<-EOT.gsub(/^\s+/, '')
           *Current Weather for #{location}:*
           Status: #{status} #{emoji}
@@ -70,6 +49,23 @@ class RabbleBot
           Cloud Cover: #{cloudcover}%
         EOT
         e.respond response
+      end
+
+      def lookup_emoji_for_weather_condition(icon)
+        ":#{@config.fetch('emojis', {}).fetch(icon, '')}:"
+      end
+
+      def parse_forecast_io_response(forecast_io_json)
+        currently = forecast_io_json.fetch('currently')
+        icon = currently['icon']
+        status = currently['summary']
+        temp = currently['temperature']
+        humidity = currently['humidity'].to_f * 100
+        windspeed = currently['windSpeed']
+        cloudcover = currently['cloudCover'].to_f * 100
+
+        # ruby has implicit returns. this will return an array containing all these variables
+        [cloudcover, humidity, icon, status, temp, windspeed]
       end
 
       def add_weather_handler
