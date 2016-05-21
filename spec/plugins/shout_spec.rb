ac13f45cddc4c16b043031bf192b8f9bfdf2e377
Tom Norris <tom.norris.iv@gmail.com>
2016-05-21 16:55:19 -0400

    add the spec file

---
+++ spec/plugins/shout_spec.rb
@@ -0,0 +1,7 @@
+require_relative('../../lib/rabblebot/basic_plugin')
+require_relative('../../lib/rabblebot/plugins/shout')
+
+RSpec.describe RabbleBot::RabbleBotPlugin::Shout do
+  it 'should get some tests' do
+  end
+end
